f004fe4f82e48a7f95b64f30bf530102058ec76d
Tamo <tamo@meilisearch.com>
2021-12-05 15:40:00 +0100

    fix part 2

--- day5/src/bin/part1.rs
+++ day5/src/bin/part1.rs
@@ -1,5 +1,5 @@
 use day5::*;
-use std::{collections::HashMap, str::FromStr};
+use std::collections::HashMap;
 
 use aoc::*;
 
--- day5/src/bin/part2.rs
+++ day5/src/bin/part2.rs
@@ -5,26 +5,31 @@ use day5::*;
 use aoc::*;
 
 fn main() {
-    let vents: Vec<_> = parser::lines::<Vent>()
-        .flat_map(|vent| vent.range)
-        .collect();
-
-    let vents = vents
-        .par_iter()
-        .progress_count(vents.len() as u64)
-        .fold(
-            || HashMap::new(),
-            |mut map, point| {
-                *map.entry(point).or_insert(0) += 1;
-                map
-            },
-        )
-        .reduce_with(|mut acc, map| {
-            map.into_iter()
-                .for_each(|(key, value)| *acc.entry(key).or_insert(0) += value);
-            acc
+    let vents = parser::lines::<Vent>()
+        .flat_map(|vent| {
+            if vent.orthogonal() {
+                Box::new(vent.range) as Box<dyn Iterator<Item = Coord<usize>>>
+            } else {
+                let Range { start, end, .. } = vent.range;
+                let x = if start.x > end.x {
+                    Box::new((end.x..=start.x).rev()) as Box<dyn Iterator<Item = usize>>
+                } else {
+                    Box::new(start.x..=end.x) as Box<dyn Iterator<Item = usize>>
+                };
+                let y = if start.y > end.y {
+                    Box::new((end.y..=start.y).rev()) as Box<dyn Iterator<Item = usize>>
+                } else {
+                    Box::new(start.y..=end.y) as Box<dyn Iterator<Item = usize>>
+                };
+                Box::new(x.zip(y).map(|tuple| tuple.into()))
+                    as Box<dyn Iterator<Item = Coord<usize>>>
+            }
         })
-        .unwrap();
+        .fold(HashMap::new(), |mut map, point| {
+            *map.entry(point).or_insert(0) += 1;
+            map
+        });
+
     answer!(
         "There is {} points that do at least two overlaps.",
         vents.values().filter(|v| **v > 1).count()
