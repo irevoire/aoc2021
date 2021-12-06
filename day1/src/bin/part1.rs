6ab2ceecd007621cc4e91ea5a20f8ece96b76e8d
Tamo <tamo@meilisearch.com>
2021-12-06 02:03:32 +0100

    remove code sample

--- day1/src/bin/part1.rs
+++ day1/src/bin/part1.rs
@@ -7,10 +7,6 @@ fn main() {
         .filter(|s| matches!(s, [a, b] if a < b))
         .count();
 
-    let (first, _) = (1, 2);
-    let [first, _] = [1, 2];
-    let [first, _] = [1, 2].as_slice();
-
     answer!(
         "{} measurements are larger than the previous measurement.",
         res
