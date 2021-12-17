ba111d9dee9aca4715d055741d81452c833836eb
Tamo <tamo@meilisearch.com>
2021-12-17 03:21:54 +0100

    add day15 part1

--- day15/src/bin/part1.rs
+++ day15/src/bin/part1.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{cmp::Reverse, collections::HashMap};
 
 use aoc::*;
 
@@ -14,67 +14,46 @@ fn main() {
 
     let grid = Grid::from(input);
 
-    println!("{}", grid);
-
-    let (width, height) = (grid.width(), grid.height());
-
     let base = Coord::at(0, 0);
-    let end = Coord::at(width - 1, height - 1);
+    let mut map = [(base, grid[base])].into_iter().collect();
 
-    let risk = best_route(
-        base,
-        &end,
-        HashSet::new(),
-        &grid,
-        0,
-        &mut (width * height * 5),
+    let risk = compute_risk_level(&grid, &mut vec![base], &mut map);
+
+    answer!(
+        "The lowest total risk of any path from the top left to the bottom right is: {}.",
+        risk - grid[base]
     );
-
-    println!("result: {}", risk - grid[base]);
 }
 
-pub fn best_route(
-    start: Coord<usize>,
-    end: &Coord<usize>,
-    mut already_explored: HashSet<Coord<usize>>,
+fn compute_risk_level(
     grid: &Grid<usize>,
-    total: usize,
-    min: &mut usize,
+    to_explore: &mut Vec<Coord>,
+    explored_cell: &mut HashMap<Coord, usize>,
 ) -> usize {
-    const DIRECTIONS: &[Direction] = &[
-        Direction::South,
+    let current_coord = to_explore.pop().unwrap();
+    let current_risk = explored_cell[&current_coord];
+
+    if current_coord == (grid.width() - 1, grid.height() - 1) {
+        return current_risk;
+    }
+
+    let iter = [
+        Direction::North,
         Direction::East,
-        // Direction::West,
-        // Direction::North,
-    ];
-    if already_explored.contains(&start) {
-        return usize::MAX;
-    }
-    let score = total + grid[start];
-    if start == *end {
-        // dbg!(*min, score);
-        *min = std::cmp::min(*min, score);
-        // println!("{}", total + grid[start]);
-        return *min;
-    }
+        Direction::West,
+        Direction::South,
+    ]
+    .into_iter()
+    .filter_map(|direction| current_coord.checked_add(direction))
+    .filter(|coord| grid.get(*coord).is_some())
+    .filter(|coord| !explored_cell.contains_key(&coord))
+    .collect::<Vec<Coord>>();
 
-    already_explored.insert(start);
-
-    if score >= *min {
-        return score;
+    for coord in iter {
+        explored_cell.insert(coord, current_risk + grid[coord]);
+        to_explore.binary_insert_by_key(coord, |coord| Reverse(explored_cell[coord]));
     }
 
-    let mut vec = DIRECTIONS
-        .iter()
-        // ensure if the coordinates are positive
-        .filter_map(|dir| start.checked_add(*dir))
-        // ensure the coordinates fit in the grid
-        .filter(|coord| grid.get(*coord).is_some())
-        .collect::<Vec<_>>();
-    vec.sort_by_key(|coord| grid[coord]);
-
-    vec.into_iter()
-        .map(|coord| best_route(coord, end, already_explored.clone(), grid, score, min))
-        .min()
-        .unwrap_or(usize::MAX)
+    // recursive tail
+    compute_risk_level(grid, to_explore, explored_cell)
 }
